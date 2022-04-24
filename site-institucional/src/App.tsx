9d964820a445faead49a91a9f1524fe2c5686db3
raijonas <rai.maciel@bandtec.com.br>
2022-04-24 14:44:54 -0300

    Tela de login, tela de not found e tela premium (em processo)

--- site-institucional/src/App.tsx
+++ site-institucional/src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home"
 import { PageNotFound } from "./pages/PageNotFound"
 import { Login } from "./pages/Login"
 import { Register } from "./pages/register/Register"
+import { Premium} from "./pages/Premium"
 
 import { MultiForm } from "./components/MultiForm"
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/page-not-found" element={<PageNotFound />} />
         <Route path="/register" element={<MultiForm />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/premium" element={<Premium />} />
       </Routes>
     </BrowserRouter>
   )
--- site-institucional/src/pages/Login.tsx
+++ site-institucional/src/pages/Login.tsx
@@ -1,7 +1,35 @@
+import { Button, Form } from 'react-bootstrap'
 import { useNavigate, Link } from 'react-router-dom'
+
+import '../styles/login.css'
 
 export function Login() {
     return (
-        <>login</>
+        <div className="page-login">
+            <section className="login-container">
+                <i className='bx bx-x fechar'></i>
+                <div className="titulo">
+                    <h1>Entrar</h1>
+                </div>
+                <div className="corpo">
+
+                    <div className="dados">
+                        <Form>
+                            <Form.Group controlId='formGroupEmail'>
+                                <Form.Control type="text" placeholder="E-mail" className="campo" />
+                            </Form.Group>
+                            <Form.Group controlId='formGroupSenha'>
+                                <Form.Control type="password" placeholder="Senha" className="campo" />
+                            </Form.Group>
+                            <Form.Group>
+                                <Button className="button2"> Entrar </Button>
+                            </Form.Group>
+                        </Form>
+
+                    </div>
+
+                </div>
+            </section>
+        </div>
     )
 }
