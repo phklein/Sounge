6d0e2f90c8298861bae5a0802b2de1e92397d88a
daniel.pederzini <daniel.pederzini@alpenet.com.br>
2022-09-23 09:35:23 -0300

    Tela principal de perfil, com post

---
+++ soungeapp/app/src/main/java/com/sounge/soungeapp/ProfilePostsFragment.kt
@@ -0,0 +1,16 @@
+package com.sounge.soungeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProfilePostsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile_posts, container, false)
+    }
+}
