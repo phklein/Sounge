a19da8d5ac2222cc97d3762672a7e18c1277e854
aleffstampini <aleff.stampini@bandtec.com.br>
2022-12-13 22:21:36 -0300

    .

--- soungeapp/app/src/main/java/com/sounge/soungeapp/fragment/TuninInfoFragment.kt
+++ soungeapp/app/src/main/java/com/sounge/soungeapp/fragment/TuninInfoFragment.kt
@@ -1,13 +1,11 @@
 package com.sounge.soungeapp.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.URLUtil
 import androidx.fragment.app.Fragment
-import com.sounge.soungeapp.databinding.FragmentMatchInfoBinding
 import com.sounge.soungeapp.databinding.FragmentTuninInfoBinding
 import com.sounge.soungeapp.response.UserMatch
 import com.sounge.soungeapp.rest.Retrofit
@@ -35,11 +33,9 @@ class MatchInfoFragment(private val userMatch: UserMatch) : Fragment() {
     }
 
     private fun setInfoUser() {
-        Log.v("test", "${userMatch}")
-
         binding.tvName.text = userMatch.name
         binding.tvAge.text = userMatch.age.toString()
-        binding.tvDescription?.text = userMatch.description
+        binding.tvDescription.text = userMatch.description
 
         if (URLUtil.isValidUrl(userMatch.profilePic)) {
             Picasso.get().load(userMatch.profilePic).into(binding.ivProfilePic)
