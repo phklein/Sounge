6008673fe9bdafc667d98b8332e463e7ee651d2d
raijonas <rai.maciel@bandtec.com.br>
2022-12-13 22:43:14 -0300

    Tela premium

--- soungeapp/app/src/main/java/com/sounge/soungeapp/actitivy/PremiumChoiceActivity.kt
+++ soungeapp/app/src/main/java/com/sounge/soungeapp/actitivy/PremiumChoiceActivity.kt
@@ -1,37 +1,134 @@
 package com.sounge.soungeapp.actitivy
 
-import android.graphics.Color.blue
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.sounge.soungeapp.R
-import com.sounge.soungeapp.databinding.ActivityPremiumBinding
+import com.sounge.soungeapp.databinding.ActionBarEditBinding.inflate
 import com.sounge.soungeapp.databinding.ActivityPremiumChoiceBinding
+import com.sounge.soungeapp.databinding.FormularioPremiumConfirmLayoutBinding
+import com.sounge.soungeapp.databinding.FormularioPremiumLayoutBinding
 import com.sounge.soungeapp.models.Offer
 
 class PremiumChoiceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPremiumChoiceBinding
+    private lateinit var dialog: AlertDialog
+
+    val offers: Array<Offer> = arrayOf(
+        Offer("Plano Bronze", "plano mensal", "25", R.drawable.headphone),
+        Offer("Plano Prata", "plano semestral", "100", R.drawable.cassetteplayer),
+        Offer("Plano Ouro", "plano anual", "150", R.drawable.jazz)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPremiumChoiceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val offers: Array<Offer> = arrayOf(
-            Offer("Plano Bronze", "plano mensal", "25", R.drawable.headphone),
-            Offer("Plano Prata", "plano semestral", "100", R.drawable.violao_cantor),
-            Offer("Plano Ouro", "plano anual", "150", R.drawable.discoteca)
-        )
 
+        addDots(offers.size)
         binding.viewPager.adapter = OnboardingAdapter(offers)
+        binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
 
+            override fun onPageSelected(position: Int) {
+                addDots(offers.size, position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
+        botaoConfirm()
 
     }
+
+    private fun testeDialog() {
+        val build = AlertDialog.Builder(this)
+            .setView(R.layout.formulario_premium_confirm_layout)
+
+        val dialogBinding: FormularioPremiumConfirmLayoutBinding =
+            FormularioPremiumConfirmLayoutBinding
+                .inflate(LayoutInflater.from(this))
+
+        dialogBinding.closeButtonFormPremiumConfirm.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialogBinding.buttonFormPremiumConfirm.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        build.setView(dialogBinding.root)
+        dialog = build.create()
+        dialog.show()
+    }
+
+    private fun dialogConfirmacaoPremium() {
+
+        val build = AlertDialog.Builder(this)
+            .setView(R.layout.formulario_premium_layout)
+
+        val dialogBinding: FormularioPremiumLayoutBinding = FormularioPremiumLayoutBinding
+            .inflate(LayoutInflater.from(this))
+
+        dialogBinding.closeButtonFormPremium.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialogBinding.confirmarButtonFormPremium.setOnClickListener {
+            dialog.dismiss()
+            testeDialog()
+        }
+
+        build.setView(dialogBinding.root)
+        dialog = build.create()
+        dialog.show()
+
+    }
+
+    private fun botaoConfirm() {
+
+        binding.buttonPremiumPlanos.setOnClickListener {
+            dialogConfirmacaoPremium()
+        }
+    }
+
+    private fun addDots(size: Int, position: Int = 0) {
+        val dots = findViewById<LinearLayout>(R.id.dots)
+        dots.removeAllViews()
+        Array(size) {
+            val textView = TextView(baseContext).apply {
+                text = getText(R.string.dotted)
+                textSize = 35f
+                setTextColor(
+                    if (position == it) {
+                        ContextCompat.getColor(baseContext, R.color.main_purple)
+                    } else {
+                        ContextCompat.getColor(baseContext, android.R.color.darker_gray)
+                    }
+                )
+            }
+            dots.addView(textView)
+        }
+    }
+
 
     private inner class OnboardingAdapter(val offers: Array<Offer>) : PagerAdapter() {
 
@@ -45,7 +142,6 @@ class PremiumChoiceActivity : AppCompatActivity() {
                 view.findViewById<TextView>(R.id.tipoPlanosOffersText).text = tipoPlano
                 view.findViewById<TextView>(R.id.valorPlanoPremiumText).text = valorPlano
                 view.findViewById<ImageView>(R.id.imagemPlanosOffers).setImageResource(logo)
-
             }
 
             container.addView(view)
