b7406c49fdb01797240c3f5c4be5b087ac9fcf09
byumisato <byumisato@gmail.com>
2022-12-13 21:06:37 -0300

    master(adjusment): Ajustes css no profile

--- site-institucional/src/pages/Home.tsx
+++ site-institucional/src/pages/Home.tsx
@@ -1,101 +1,146 @@
-import React, { useEffect } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 // import Swal from 'sweetalert2'
 
-import '../styles/home.css'
+import "../styles/home.css";
 
-import EventOne from '../assets/img/event-one.png'
-import EventTwo from '../assets/img/event-two.png'
-import EventThree from '../assets/img/event-three.png'
-import PubOne from '../assets/img/pub-one.png'
-import PubTwo from '../assets/img/pub-two.png'
-import PubThree from '../assets/img/pub-three.png'
+import EventOne from "../assets/img/event-one.png";
+import EventTwo from "../assets/img/event-two.png";
+import EventThree from "../assets/img/event-three.png";
+import PubOne from "../assets/img/pub-one.png";
+import PubTwo from "../assets/img/pub-two.png";
+import PubThree from "../assets/img/pub-three.png";
 
-import { NavBar } from '../components/Navbar'
-import { Footer } from '../components/Footer'
-import { Buttonaa } from '../components/Buttonaa'
+import { NavBar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import { Buttonaa } from "../components/Buttonaa";
 
-const cardStyles = { 
-    publicationStyle1: { background: `url(${PubOne})`, backgroundSize: 'cover', backgroundPosition: 'center' },
-    publicationStyle2: { background: `url(${PubTwo})`, backgroundSize: 'cover', backgroundPosition: 'center' },
-    publicationStyle3: { background: `url(${PubThree})`, backgroundSize: 'cover', backgroundPosition: 'center' }
-}
+const cardStyles = {
+  publicationStyle1: {
+    background: `url(${PubOne})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  publicationStyle2: {
+    background: `url(${PubTwo})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  publicationStyle3: {
+    background: `url(${PubThree})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+};
 
 export function Home() {
+  useEffect(() => {
+    console.log(cardStyles);
+  }, []);
 
-    useEffect(() => {
-        console.log(cardStyles)
-    }, [])
-
-    return (
-        <div className="page-init">
-            <NavBar />
-            <section className="banner">
-                <h1>“Tocar uma nota equivocada é insignificante.<br />Tocar sem paixão é imperdoável”</h1>
-                <p>Ludwing Van Beethoven</p>
-                <Link to="/login"><button className="btn-entrar">Entrar</button></Link>   
-            </section>
-            <section className="presentation">
-                <div className="container">
-                    <div className="presentation-container">
-                        <div className="presentation-item">
-                            <h1>Realize</h1>
-                            <h1>Negocie</h1>
-                            <h1>Compartilhe</h1>
-                            <h2>Divulgue o seu trabalho e alcance a oportunidade que tanto almeja!</h2>
-                            <h2>Divulgue o seu trabalho e alcance a oportunidade que tanto almeja!</h2>
-                            <h2>Divulgue o seu trabalho e alcance a oportunidade que tanto almeja!</h2>
-                        </div>
-                        <div className="presentation-item">
-                            <div className="img-presentation">
-                                <h2>Realize</h2>
-                            </div>
-                            <div className="img-presentation">
-                                <h2>Negocie</h2>
-                            </div>
-                            <div className="img-presentation">
-                                <h2>Compartilhe</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="player">
-                <h1>Compartilhe <span>seu som!</span></h1>
-                <iframe className="container" src="https://open.spotify.com/embed/playlist/2M2aj5IeLoaNlu4wgNxvcH?utm_source=generator" width="100%" height="100%" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-            </section>
-            <section className="events">
-                <h1>Os eventos mais esperados</h1>
-                <div className="container events-list">
-                    <div className="events-item">
-                        <img src={EventOne} alt="" />
-                        <h2>Lollapaloza Brazil</h2>
-                        <p>qui., 03 de maio</p>
-                        <Buttonaa>rspv</Buttonaa>
-                    </div>
-                    <div className="events-item">
-                        <img src={EventTwo} alt="" />
-                        <h2>Rock in Rio</h2>
-                        <p>sáb., 20 de jan.</p>
-                        <Buttonaa>rspv</Buttonaa>
-                    </div>
-                    <div className="events-item">
-                        <img src={EventThree} alt="" />
-                        <h2>Coachella</h2>
-                        <p>sáb., 10 de agosto</p>
-                        <Buttonaa>rspv</Buttonaa>
-                    </div>
-                </div>
-            </section>
-            <section className="publications">
-                <h1>Veja publicações do momento</h1>
-                <div className="container publications-list">
-                    <div className="publication-item" style={cardStyles.publicationStyle1}></div>
-                    <div className="publication-item" style={cardStyles.publicationStyle2}></div>
-                    <div className="publication-item" style={cardStyles.publicationStyle3}></div>
-                </div>
-            </section>
-            <Footer />
+  return (
+    <div className="page-init">
+      <NavBar />
+      <section className="banner">
+        <h1>
+          “Tocar uma nota equivocada é insignificante.
+          <br />
+          Tocar sem paixão é imperdoável”
+        </h1>
+        <p>Ludwing Van Beethoven</p>
+        <Link to="/login">
+          <button className="btn-entrar">Entrar</button>
+        </Link>
+      </section>
+      {/* <section className="presentation">
+        <div className="container">
+          <div className="presentation-container">
+            <div className="presentation-item">
+              <h1>Realize</h1>
+              <h1>Negocie</h1>
+              <h1>Compartilhe</h1>
+              <h2>
+                Divulgue o seu trabalho e alcance a oportunidade que tanto
+                almeja!
+              </h2>
+              <h2>
+                Divulgue o seu trabalho e alcance a oportunidade que tanto
+                almeja!
+              </h2>
+              <h2>
+                Divulgue o seu trabalho e alcance a oportunidade que tanto
+                almeja!
+              </h2>
+            </div>
+            <div className="presentation-item">
+              <div className="img-presentation">
+                <h2>Realize</h2>
+              </div>
+              <div className="img-presentation">
+                <h2>Negocie</h2>
+              </div>
+              <div className="img-presentation">
+                <h2>Compartilhe</h2>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </section> */}
+      <section className="player">
+        <h1>
+          Compartilhe <span>seu som!</span>
+        </h1>
+        <iframe
+          className="containerSpotify"
+          src="https://open.spotify.com/embed/playlist/2M2aj5IeLoaNlu4wgNxvcH?utm_source=generator"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allowFullScreen={true}
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      </section>
+      <section className="events">
+        <h1>Os eventos mais esperados</h1>
+        <div className="container events-list">
+          <div className="events-item">
+            <img src={EventOne} alt="" />
+            <h2>Lollapaloza Brazil</h2>
+            <p>qui., 03 de maio</p>
+            <Buttonaa>rspv</Buttonaa>
+          </div>
+          <div className="events-item">
+            <img src={EventTwo} alt="" />
+            <h2>Rock in Rio</h2>
+            <p>sáb., 20 de jan.</p>
+            <Buttonaa>rspv</Buttonaa>
+          </div>
+          <div className="events-item">
+            <img src={EventThree} alt="" />
+            <h2>Coachella</h2>
+            <p>sáb., 10 de agosto</p>
+            <Buttonaa>rspv</Buttonaa>
+          </div>
+        </div>
+      </section>
+      <section className="publications">
+        <h1>Veja publicações do momento</h1>
+        <div className="container publications-list">
+          <div
+            className="publication-item"
+            style={cardStyles.publicationStyle1}
+          ></div>
+          <div
+            className="publication-item"
+            style={cardStyles.publicationStyle2}
+          ></div>
+          <div
+            className="publication-item"
+            style={cardStyles.publicationStyle3}
+          ></div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
 }
--- site-institucional/src/pages/profile/Profile.tsx
+++ site-institucional/src/pages/profile/Profile.tsx
@@ -1,118 +1,128 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import React, { useEffect, useState } from "react"
-import {useParams} from "react-router-dom"
+import "../../styles/profile.css";
 
-import "../../styles/profile.css"
+import UserRoute from "../../routes/UserRoute";
 
-import UserRoute from "../../routes/UserRoute"
+import PictureUpdateRequestDto from "../../dto/request/PictureUpdateRequestDto";
+import UserProfileResponseDto from "../../dto/response/UserProfileResponseDto";
 
-import PictureUpdateRequestDto from "../../dto/request/PictureUpdateRequestDto"
-import UserProfileResponseDto from "../../dto/response/UserProfileResponseDto"
-
-import { NavBar } from "../../components/Navbar"
-import { BtnSintonizar } from "../../components/BtnSintonizar"
-import { PageIntroduction } from "../../components/PageIntroduction"
-import { PostPage } from "../../components/PostPage"
+import { NavBar } from "../../components/Navbar";
+import { BtnSintonizar } from "../../components/BtnSintonizar";
+import { PageIntroduction } from "../../components/PageIntroduction";
+import { PostPage } from "../../components/PostPage";
 
 export function Profile() {
-  const [userProfile, setUserProfile] = useState<UserProfileResponseDto>()
-  const {id} = useParams()
+  const [userProfile, setUserProfile] = useState<UserProfileResponseDto>();
+  const { id } = useParams();
 
   const convertImageToByteArray = (image: any) => {
-    let reader = new FileReader()
+    let reader = new FileReader();
 
-    let fileByArray: any[] = []
+    let fileByArray: any[] = [];
 
-    reader.readAsArrayBuffer(image)
+    reader.readAsArrayBuffer(image);
 
     reader.onload = (e) => {
       if (e.target?.readyState == FileReader.DONE) {
-        let arrayBuffer = (e.target as FileReader).result as ArrayBuffer
-        let array = new Uint8Array(arrayBuffer)
+        let arrayBuffer = (e.target as FileReader).result as ArrayBuffer;
+        let array = new Uint8Array(arrayBuffer);
 
         for (let i = 0; i < array.length; i++) {
-          fileByArray.push(array[i])
+          fileByArray.push(array[i]);
         }
       }
-      
-      const reader = new FileReader()
 
-      reader.readAsDataURL(image)
+      const reader = new FileReader();
+
+      reader.readAsDataURL(image);
       reader.onload = () => {
-        console.log(reader.result)
+        console.log(reader.result);
 
-        // TODO: BRUNA
         const request: PictureUpdateRequestDto = {
           profilePic: reader.result,
-          banner: userProfile?.banner
-        }
+          banner: userProfile?.banner,
+        };
 
-        UserRoute.updateSimplePicture(id, request).then(res => {
-          if (res.status === 200) {
-            alert('Banner atualizado com sucesso!')
-            window.location.reload()
-          }
-        }).catch(err => {
-          console.log(err)
-        })
-      }
-    } 
-  }
+        UserRoute.updateSimplePicture(id, request)
+          .then((res) => {
+            if (res.status === 200) {
+              alert("Banner atualizado com sucesso!");
+              window.location.reload();
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+    };
+  };
 
   const onChange = async (e: React.FormEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement
-    
+    const target = e.target as HTMLInputElement;
+
     if (target.files && target.files.length) {
-      const file = target.files[0]
+      const file = target.files[0];
 
-      console.log(file)
+      console.log(file);
 
-      convertImageToByteArray(file)
+      convertImageToByteArray(file);
     }
-  }
+  };
 
   useEffect(() => {
     const getUserProfile = async () => {
-      
-      // TODO: BRUNA
-      const queryString = window.location.search
-      const urlParams = new URLSearchParams(queryString)
-      const viewerId = urlParams.get('viewerId')
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const viewerId = urlParams.get("viewerId");
 
       if (viewerId) {
-        await UserRoute.getProfileForId(id, viewerId).then(res => {
-          if (res.status === 200) {
-            console.log(res.data)
-            setUserProfile(res.data)
-          }          
-        }).catch(err => {
-          console.log(err)
-        })
+        await UserRoute.getProfileForId(id, viewerId)
+          .then((res) => {
+            if (res.status === 200) {
+              console.log(res.data);
+              setUserProfile(res.data);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
 
-        console.log(userProfile)
+        console.log(userProfile);
       }
-    }
+    };
 
-    getUserProfile()
-  }, [])
+    getUserProfile();
+  }, []);
 
   return (
     <>
       <NavBar isbtnRegisterOff />
       <div className="background-profile">
         <div className="container">
-        <div className="profile-container">
-          <div className="profile-cap" style={{backgroundSize: 'cover', backgroundPosition: 'center' ,backgroundImage: `url(${userProfile?.banner})`}}>
-            <div className="btn-update-picture">aaaaaaa</div>
-          </div>
-          <div className="profile-details">
-            <div className="profile-picture" style={{backgroundImage: `url(${userProfile?.profilePic})`}}>
-              <div className="circle-online"></div>
+          <div className="profile-container">
+            <div
+              className="profile-cap"
+              style={{
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundImage: `url(${userProfile?.banner})`,
+              }}
+            >
+              <div className="btn-update-picture">aaaaaaa</div>
             </div>
-            <h2 className="profile-user-name">{userProfile?.name}</h2>
-            <h4 className="typography">{userProfile?.description}</h4>
-          </div>
-          <div className="father-menu">
+            <div className="profile-details">
+              <div
+                className="profile-picture"
+                style={{ backgroundImage: `url(${userProfile?.profilePic})` }}
+              >
+                <div className="circle-online"></div>
+              </div>
+              <h2 className="profile-user-name">{userProfile?.name}</h2>
+              <h4 className="typography">{userProfile?.description}</h4>
+            </div>
+            <div className="father-menu">
               <div className="menu-profile">
                 <h5 className="typography">Feed</h5>
                 <h5 className="typography">Sobre</h5>
@@ -121,53 +131,45 @@ export function Profile() {
                 <h5 className="typography">Mais</h5>
                 <i className="bx bxs-down-arrow seta-baixo"></i>
               </div>
-            <div className="btn-space">
-              <BtnSintonizar />
+              <div className="btn-space">
+                <BtnSintonizar />
+              </div>
             </div>
           </div>
         </div>
-        </div>
       </div>
-      <input 
+      <input
         type="file"
-        name="banner" 
-        id="banner" 
+        name="banner"
+        id="banner"
         accept="image/png, image/jpeg"
         onChange={onChange}
       />
       <div className="container">
         <div className="profile-container">
           <div className="test">
-
             <div className="background-intro">
               <h1 className="intro">Habilidades</h1>
-                {
-                  userProfile?.roles.map(skill => (
-                    <PageIntroduction
-                      key={skill.id}
-                      name={skill.name}
-                    />
-                  ))
-                }
+              {userProfile?.roles.map((skill) => (
+                <PageIntroduction key={skill.id} name={skill.name} />
+              ))}
             </div>
             <div className="post-container">
-              {
-                userProfile?.postList.map(post => (
-                  <PostPage
-                    key={post.id}
-                    text={post.text}
-                    mediaUrl={post.mediaUrl}
-                    hoursPast={post.hoursPast}
-                    user={post.user}
-                    likeCount={post.likeCount}
-                    commentCount={post.commentCount}
-                  />
-                ))
-              }
+              {userProfile?.postList.map((post) => (
+                <PostPage
+                  key={post.id}
+                  text={post.text}
+                  mediaUrl={post.mediaUrl}
+                  hoursPast={post.hoursPast}
+                  user={post.user}
+                  likeCount={post.likeCount}
+                  commentCount={post.commentCount}
+                />
+              ))}
             </div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
